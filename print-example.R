277760b70ca777ab7b3135baa02cdb13f4111133
Ansus <susanna.andrea@gmail.com>
2019-07-30 18:08:56 +0200

    Added Order paragraph

--- print-example.R
+++ print-example.R
@@ -1,1 +1,21 @@
-println()
+
+library(tidyverse)
+df= read.csv('data.csv')
+df= df %>% 
+  mutate(InvoiceDateTime= as.POSIXct(strptime(InvoiceDate, format = "%m/%d/%Y %H:%M")),
+         Date= as.Date(InvoiceDate, format = "%m/%d/%Y %H:%M"),
+         WeekDay= weekdays(as.Date(InvoiceDate, format = "%m/%d/%Y %H:%M")),
+         Hour= as.integer(strftime(InvoiceDateTime, format="%H")),
+         Day= as.integer(strftime(InvoiceDateTime, format="%d")),
+         Month= as.integer(strftime(InvoiceDateTime, format="%m")),
+         Year= as.integer(strftime(InvoiceDateTime, format="%Y"))
+  )
+
+df$WeekDay= df$WeekDay %>% 
+  substr(1,3) %>% 
+  factor(levels= c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"))
+
+
+
+
+
